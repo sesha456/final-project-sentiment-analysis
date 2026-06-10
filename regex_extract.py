27694904d6d41ac3662b90cb54cb53035a839e87
sesha456 <seshasivasankar@gmail.com>
2026-06-10 10:36:55 -0500

    Fix prediction bug: prevent list accumulation in sentiment analysis
    - Removed global data list that was causing predictions to accumulate
    - Modified regex_extract.py to return fresh data for each prediction
    - Fixed issue where model was predicting on all previous inputs instead of just current input

--- regex_extract.py
+++ regex_extract.py
@@ -5,7 +5,6 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 
-data = []
 def user_data(value):
     value1 = re.sub('[^a-zA-Z]', ' ', value)
     value1 = value1.lower()
@@ -14,5 +13,4 @@ def user_data(value):
     ps = PorterStemmer()
     value1 = [ps.stem(word) for word in value1]
     value1 = ' '.join(value1)
-    data.append(value1)
-    return data
+    return [value1]
